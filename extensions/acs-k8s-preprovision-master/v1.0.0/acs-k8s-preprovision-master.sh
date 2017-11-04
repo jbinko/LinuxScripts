364bd2fad5811363b35618288a7a1baa2904cbf1
Jiri Binko <jiri_binko@msn.com>
2017-11-04 11:56:26 +0100

    C

--- extensions/acs-k8s-preprovision-master/v1.0.0/acs-k8s-preprovision-master.sh
+++ extensions/acs-k8s-preprovision-master/v1.0.0/acs-k8s-preprovision-master.sh
@@ -1,1 +1,69 @@
-ls
+#!/bin/bash
+# https://raw.githubusercontent.com/Azure/acs-engine/master/extensions/microsoft-oms-agent-k8s/v1/microsoft-oms-agent-k8s.sh
+
+set -e
+[ "$DEBUG" == 'true' ] && set -x
+
+parameters=$(echo $1 | base64 -d -)
+
+log() {
+  echo "`date +'[%Y-%m-%d %H:%M:%S:%N %Z]'` $1"
+}
+
+get_param() {
+  local param=$1
+  echo $(echo "$parameters" | jq ".$param" -r)
+}
+
+install_script_dependencies() {
+  log ''
+  log 'Installing script dependencies'
+  log ''
+
+  # Install jq to obtain the input parameters
+  log 'Installing jq'
+  log ''
+  sudo apt-get -y install jq
+  log ''
+
+  log 'done'
+  log ''
+}
+
+cleanup_script_dependencies() {
+  log ''
+  log 'Removing script dependencies'
+  log ''
+
+  log 'Removing jq'
+  log ''
+  sudo apt-get -y remove jq
+  log ''
+
+  log 'done'
+  log ''
+}
+
+preprovision() {
+  log ''
+  log 'preprovisioning'
+  log ''
+
+  #local wsid=$(get_param 'WSID')
+  #local key=$(get_param 'KEY')
+  
+  log 'done'
+  log ''
+}
+
+log ''
+log 'acs-k8s-preprovision-master'
+log '--------------------------------------------------'
+
+install_script_dependencies
+preprovision
+cleanup_script_dependencies
+
+log ''
+log 'done'
+log ''
