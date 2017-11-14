3aff0534173ead069408a98e37bdde1e0ca91860
Jiri Binko <jiri_binko@msn.com>
2017-11-14 16:29:09 +0100

    C

--- acs-k8s-manifests-env.sh
+++ acs-k8s-manifests-env.sh
@@ -18,11 +18,11 @@ adjust_manifests() {
   sed -i "s,command:,env:\\n        - name: https_proxy\\n          value: $PROXY\\n        - name: http_proxy\\n          value: $PROXY\\n      command:",g /etc/kubernetes/manifests/kube-controller-manager.yaml
   sed -i "s,--v=2,--v=1,g" /etc/kubernetes/manifests/kube-controller-manager.yaml
   #sed -i "s,command:,env:\\n        - name: https_proxy\\n          value: $PROXY\\n        - name: http_proxy\\n          value: $PROXY\\n      command:",g /etc/kubernetes/manifests/kube-apiserver.yaml
-  #sed -i "s,--v=4,--v=1,g" /etc/kubernetes/manifests/kube-apiserver.yaml
+  sed -i "s,--v=4,--v=1,g" /etc/kubernetes/manifests/kube-apiserver.yaml
   #sed -i "s,resources:,env:\\n    - name: https_proxy\\n      value: $PROXY\\n    - name: http_proxy\\n      value: $PROXY\\n    resources:",g /etc/kubernetes/manifests/kube-addon-manager.yaml
   #sed -i "s,--v=2,--v=1,g" /etc/kubernetes/manifests/kube-addon-manager.yaml
   #sed -i "s,command:,env:\\n        - name: https_proxy\\n          value: $PROXY\\n        - name: http_proxy\\n          value: $PROXY\\n      command:",g /etc/kubernetes/manifests/kube-scheduler.yaml
-  #sed -i "s,--v=2,--v=1,g" /etc/kubernetes/manifests/kube-scheduler.yaml
+  sed -i "s,--v=2,--v=1,g" /etc/kubernetes/manifests/kube-scheduler.yaml
 
   log ''
   log 'close'
