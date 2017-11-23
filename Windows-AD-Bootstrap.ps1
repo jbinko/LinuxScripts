39cae1a9b78c62e104619a556147b249341dc02a
Jiri Binko <jiri_binko@msn.com>
2017-11-24 00:45:02 +0100

    C

--- Windows-AD-Bootstrap.ps1
+++ Windows-AD-Bootstrap.ps1
@@ -10,9 +10,9 @@
  )
 
 # Set proxy				
-reg add 'HKLM\\SOFTWARE\\Policies\\Microsoft\\Windows\\CurrentVersion\\Internet Settings' /v ProxySettingsPerUser /t REG_DWORD /d 0 /f
-reg add 'HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings' /v ProxyEnable /t REG_DWORD /d 1 /f
-reg add 'HKLM\\Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings' /v ProxyServer /t REG_SZ /d $webProxy /f
+reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settings' /v ProxySettingsPerUser /t REG_DWORD /d 0 /f
+reg add 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ProxyEnable /t REG_DWORD /d 1 /f
+reg add 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ProxyServer /t REG_SZ /d $webProxy /f
 netsh winhttp set proxy $webProxy
 
 # Prepare AD Data Disk
