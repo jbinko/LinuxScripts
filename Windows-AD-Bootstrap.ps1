dad1c5cda932ddb6d3b911cbf52a54d1a9c41283
Jiri Binko <jiri_binko@msn.com>
2017-11-24 22:23:28 +0100

    C

--- Windows-AD-Bootstrap.ps1
+++ Windows-AD-Bootstrap.ps1
@@ -47,7 +47,7 @@ $adPassword = ConvertTo-SecureString $adPasswordText -AsPlainText -Force
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "F:\NTDS" -DomainMode "Win2012R2" -DomainName $domainName -DomainNetbiosName $domainNetbiosName -ForestMode "Win2012R2" -InstallDns:$true -LogPath "F:\NTDS" -NoRebootOnCompletion:$False -SysvolPath "F:\SYSVOL" -Force:$true -SafeModeAdministratorPassword $adPassword
 Set-DnsServerDiagnostics -All $true
 
-$domainAdminCred = New-Object System.Management.Automation.PSCredential ($domainAdminName, $domainAdminPassword)
+$domainAdminCred = New-Object System.Management.Automation.PSCredential ($domainAdminName, ( $domainAdminPassword | ConvertTo-SecureString -asPlainText -Force ))
 $j = Start-Job -credential $domainAdminCred -ScriptBlock {
 
 	# Create new OU
