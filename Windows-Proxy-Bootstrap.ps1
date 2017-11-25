4fb683e3b1655e3965a11dbdab7b54ef539e1bec
Jiri Binko <jiri_binko@msn.com>
2017-11-25 13:18:02 +0100

    C

--- Windows-Proxy-Bootstrap.ps1
+++ Windows-Proxy-Bootstrap.ps1
@@ -1,6 +1,10 @@
  Param(
 	 [Parameter(Mandatory=$True,Position=1)]
-	 [string]$webProxy
+	 [string]$webProxy,
+	 [Parameter(Mandatory=$True,Position=2)]
+	 [string]$dscFileURL,
+	 [Parameter(Mandatory=$True,Position=3)]
+	 [string]$dscFileTargetPath
  )
 
 # Set proxy				
@@ -8,3 +12,8 @@ reg add 'HKLM\SOFTWARE\Policies\Microsoft\Windows\CurrentVersion\Internet Settin
 reg add 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ProxyEnable /t REG_DWORD /d 1 /f
 reg add 'HKLM\Software\Microsoft\Windows\CurrentVersion\Internet Settings' /v ProxyServer /t REG_SZ /d $webProxy /f
 netsh winhttp set proxy $webProxy
+
+# Download DSC
+$webClient = New-Object System.Net.WebClient
+$webClient.Proxy = New-Object System.Net.WebProxy($webProxy, $true)
+$webClient.DownloadFile($dscFileURL, $dscFileTargetPath)
